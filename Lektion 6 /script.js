33d12896b0cba0dd734f68af67d6d2779fbc0442
Lotta <charlotte.a.nyman@gmail.com>
2022-10-04 08:59:00 +0200

    done some css and js

--- Lektion 6 /script.js
+++ Lektion 6 /script.js
@@ -25,20 +25,23 @@ document.getElementById("SubmitGuess").onclick = function () {
     // alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESS ");
     document.getElementById(
       "right"
-    ).innerHTML = `YEY!! It only took ${guess} times to guess right!`;
+    ).innerHTML = `YEY!! It only took ${guess} times to guess right! `;
   } else if (randomGuess > random) {
     console.log(" to small guess");
+    // guess++;
+    // alert("OOPS SORRY!! TRY A SMALLER NUMBER");
     guess++;
-    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
-    guess++;
-    // document.getElementById("wrong").innerHTML = "SORRY TRY A SMALLER NR!!";
+
+    document.getElementById("wrong").innerHTML = "SORRY TRY A SMALLER NR!!";
   } else {
     console.log(" to great guess");
     {
-      guess++;
-      alert("OOPS SORRY!! TRY A GREATER NUMBER");
       // guess++;
-      // document.getElementById("wrong").innerHTML = "SORRY TRY A GREATER NR!!";
+      // alert("OOPS SORRY!! TRY A GREATER NUMBER");
+
+      guess++;
+
+      document.getElementById("wrong").innerHTML = "SORRY TRY A GREATER NR!!";
     }
   }
 };
